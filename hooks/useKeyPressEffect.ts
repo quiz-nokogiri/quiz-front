0a764b7b0384f2f86e345d7d63dbe93da8840209
kubo-hide-kun <hide44041@gmail.com>
2023-02-05 17:58:26 +0900

    fix: enable repeat of keypress :bug:

--- hooks/useKeyPressEffect.ts
+++ hooks/useKeyPressEffect.ts
@@ -3,19 +3,22 @@ import { DependencyList, useEffect } from "react";
 export const useKeyPressEffect = (
   targetKey: string,
   onKeyPressed: VoidFunction,
-  deps: DependencyList = []
+  deps: DependencyList = [],
+  options = { enableRepeat: false }
 ) => {
   useEffect(() => {
     const downHandler = (event: KeyboardEvent) => {
+      if (!options.enableRepeat && event.repeat) {
+        return;
+      }
       if (event.key === targetKey) {
         onKeyPressed();
       }
     };
 
     window.addEventListener("keydown", downHandler);
-
     return () => {
       window.removeEventListener("keydown", downHandler);
     };
-  }, [onKeyPressed, targetKey, deps]);
+  }, [onKeyPressed, targetKey, deps, options.enableRepeat]);
 };
