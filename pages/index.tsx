6b2267c9b441c521608fa6f8e1336f59aba04ea1
nakami3316 <nakami3316@gmail.com>
2023-03-05 23:26:44 +0900

    add head: display statement of the search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import Link from 'next/link'
+import Head from 'next/head'
 import styles from "../styles/Home.module.css";
 
 export default function RootPage() {
   return (
-    <div className={styles.whole}>
-      <h1 className={styles.theme}>
-        無限クイズ
-      </h1>
-      <div className={styles.start}>
-        <Link href="/questions">START</Link>
+    <>
+      <Head>
+        <title>UnlimitedQuiz</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="UnlimitedQuiz　無限クイズ 　- ワクワクする人生に。クイズを楽しみ、暇つぶししながら知識、好奇心を手に入れましょう。" />
+        <meta name="keywords" content="クイズ,無限,暇つぶし,スマートフォン,スマホ,UnlimitedQuiz,無限クイズ" />
+        <title>UnlimitedQuiz</title>
+      </Head><div className={styles.whole}>
+        <h1 className={styles.theme}>
+          無限クイズ
+        </h1>
+        <div className={styles.start}>
+          <Link href="/questions">START</Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
