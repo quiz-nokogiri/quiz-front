130b11d0555e0fb91b60fb0c6a9fab9e28f6b6f0
nakami3316 <nakami3316@gmail.com>
2023-03-08 18:05:31 +0900

    chenge design of index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,26 +16,39 @@ export default function RootPage() {
 
   return (
     <>
-    <div {...handlers}>
       <Head>
         <title>UnlimitedQuiz</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="UnlimitedQuiz　無限クイズ 　- ワクワクする人生に。クイズを楽しみ、暇つぶししながら知識、好奇心を手に入れましょう。" />
         <meta name="keywords" content="クイズ,無限,暇つぶし,スマートフォン,スマホ,UnlimitedQuiz,無限クイズ" />
       </Head>
-      <h1 className={styles.theme}>
-        UnlimitedQuiz
-      </h1>
-      <h2>
-        全ての操作がスワイプで操作が行えます。<br />
-        上にスワイプしてクイズSTART！
-      </h2>
-      <div className={styles.whole}>
-        <div className={styles.start}>
-          <Link href="/quizzes" className={styles.bo}>START</Link>
+      <div {...handlers} className={styles.entire}>
+        <h1 className={styles.title}>
+          UnlimitedQuiz
+        </h1>
+        <h2>
+          全ての操作がスワイプで操作が行えます。<br />
+          上にスワイプしてクイズSTART！
+        </h2>
+        <div className={styles.whole}>
+          <div className={styles.start}>
+            <Link href="/quizzes" className={styles.bo}>START</Link>
+          </div>
         </div>
       </div>
-    </div>
+      <div className={styles.fixed_footer_menu}>
+        <ul>
+          <li>
+            <Link href="./">
+              <div className={styles.footer_icon}>
+                <i className="fa-solid fa-house"></i>
+              </div>
+              <p>Home</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
--- pages/quizzes.tsx
+++ pages/quizzes.tsx
@@ -192,8 +192,8 @@ export default function QuestionsPage() {
   return (
     <>
       <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
         <title>UnlimitedQuiz</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="UnlimitedQuiz　無限クイズ 　- ワクワクする人生に。クイズを楽しみ、暇つぶししながら知識、好奇心を手に入れましょう。" />
         <meta name="keywords" content="クイズ,無限,暇つぶし,スマートフォン,スマホ,UnlimitedQuiz,無限クイズ" />
