756790483015b1f9eac8080e4cdeb52c815ef010
nakami3316 <nakami3316@gmail.com>
2023-03-08 20:52:15 +0900

    add site_name for seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,7 @@ export default function RootPage() {
         <title>UnlimitedQuiz</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:site_name" content="UnlimitedQuiz" />
         <meta name="description" content="UnlimitedQuiz　無限クイズ 　- ワクワクする人生に。クイズを楽しみ、暇つぶししながら知識、好奇心を手に入れましょう。" />
         <meta name="keywords" content="クイズ,無限,暇つぶし,スマートフォン,スマホ,UnlimitedQuiz,無限クイズ" />
       </Head>
@@ -42,7 +43,7 @@ export default function RootPage() {
           <li>
             <Link href="./">
               <div className={styles.footer_icon}>
-                <i className="fa-solid fa-house"></i>
+                <i className="fa-solid fa-house" />
               </div>
               <p>Home</p>
             </Link>
