9ee0404dfc99450761eb94866538d7fa748c2f47
nakami3316 <nakami3316@gmail.com>
2023-03-08 17:40:20 +0900

    chenge color

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function RootPage() {
     },
     trackMouse: true, //マウス操作でのスワイプを許可する場合はtrue
   });
-  
+
   return (
     <>
     <div {...handlers}>
@@ -22,7 +22,6 @@ export default function RootPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="UnlimitedQuiz　無限クイズ 　- ワクワクする人生に。クイズを楽しみ、暇つぶししながら知識、好奇心を手に入れましょう。" />
         <meta name="keywords" content="クイズ,無限,暇つぶし,スマートフォン,スマホ,UnlimitedQuiz,無限クイズ" />
-        <title>UnlimitedQuiz</title>
       </Head>
       <h1 className={styles.theme}>
         UnlimitedQuiz
