0c5eeed8acd4fb5f5d59eae8bd889e563174552b
nakami3316 <nakami3316@gmail.com>
2023-03-11 19:59:14 +0900

    add swipe animation

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,8 +7,8 @@ export default class Document extends NextDocument {
         <Head>
             <title>UnlimitedQuiz</title>
             <meta property="og:site_name" content="UnlimitedQuiz" />
-            <meta name="keywords" content="クイズ,無限,暇つぶし,スマートフォン,スマホ,UnlimitedQuiz,無限クイズ" />
-            <meta name="description" content="UnlimitedQuiz　無限クイズ 　- ワクワクする人生に。クイズを楽しみ、暇つぶししながら知識、好奇心を手に入れましょう。" />
+            <meta name="keywords" content="クイズ,無制限,暇つぶし,スマートフォン,スマホ,UnlimitedQuiz,無制限クイズ" />
+            <meta name="description" content="UnlimitedQuiz　無制限クイズ 　- ワクワクする人生に。クイズを楽しみ、暇つぶししながら知識、好奇心を手に入れましょう。" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta charSet="UTF-8" />
             <meta name="msapplication-TileColor" content="#da532c" />
